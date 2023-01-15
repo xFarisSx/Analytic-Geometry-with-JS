204da09a442d11f5dab46e43a4494a6f8c838171
xFarisSx <faristaleek@gmail.com>
2023-01-15 19:51:51 +0300

    fixes

--- script.js
+++ script.js
@@ -28,6 +28,13 @@ let keys = {
 
 let pressPos = [0, 0];
 
+let a = 1;
+let squareX1 = 0;
+let squareX2 = 0;
+let squareY = 0;
+let cubeH = 100;
+let cubeW = 200;
+
 let pressed = false;
 let x = 0;
 let y = 0;
@@ -69,7 +76,7 @@ function drawPoints(points) {
 function makePoints(start, end, func, moveX) {
   let points = [];
   for (let x = start; x <= end; x += 1) {
-    y = func(x-moveX);
+    y = func(x - moveX);
     points.push([x, y]);
   }
   x1 = points[0][0];
@@ -78,50 +85,166 @@ function makePoints(start, end, func, moveX) {
 }
 
 function drawSquare(x, y, sWidth, front) {
-  let moveX = x
+  let m2 = -1 / a;
+  let moveX = x;
   let oran = 1;
-  let x1 = x-sWidth/2
-  let x2 = x+sWidth/2
-  let ortaX = (x1+x2)/2
-  let y1 = y-sWidth/2
-  let y2 = y+sWidth/2
-  let ortaY = (y1+y2)/2
-  makePoints(ortaX, ortaX + sWidth / (oran * 2), (x) => {
-    return x + y ;
-  }, moveX);
+  let x1 = x - sWidth / 2;
+  let x2 = x + sWidth / 2;
+  let ortaX = (x1 + x2) / 2;
+  let y1 = y - sWidth / 2;
+  let y2 = y + sWidth / 2;
+  let ortaY = (y1 + y2) / 2;
+  makePoints(
+    ortaX,
+    ortaX + sWidth / (oran * 2),
+    (x) => {
+      return a * x + y;
+    },
+    moveX
+  );
   if (true) {
-    makePoints(ortaX - sWidth / (oran * 2), ortaX, (x) => {
-      return x + sWidth + y;
-    }, moveX);
-    makePoints(ortaX, ortaX + sWidth / (oran * 2), (x) => {
-      return -x + sWidth + y;
-    }, moveX);
+    makePoints(
+      ortaX - sWidth / (oran * 2),
+      ortaX,
+      (x) => {
+        return a * x + sWidth + y;
+      },
+      moveX
+    );
+    makePoints(
+      ortaX,
+      ortaX + sWidth / (oran * 2),
+      (x) => {
+        return m2 * x + sWidth + y;
+      },
+      moveX
+    );
   }
-  makePoints(ortaX - sWidth / (oran * 2), ortaX, (x) => {
-    return -x + y;
-  },moveX);
+  makePoints(
+    ortaX - sWidth / (oran * 2),
+    ortaX,
+    (x) => {
+      return m2 * x + y;
+    },
+    moveX
+  );
 }
 
-function drawCube(x, x2,y, y2, width) {
-  let ortaX = (x + x2)/2
-  let ortaY = (y + y2)/2
-  
+function drawCube(x, x2, y, y2, width) {
+  let ortaX = (x + x2) / 2;
+  let ortaY = (y + y2) / 2;
 
   drawSquare(x2, y2, width, false);
   drawSquare(x, y, width, true);
-  drawLine(ortaX-width / 2, -y-width/2, ortaX-width / 2, -(y2+width/2));
-  drawLine(ortaX+ width / 2,- y-width/2,ortaX+ width / 2, -(y2+width/2));
+  drawLine(
+    ortaX - width / 2,
+    -y - width / 2,
+    ortaX - width / 2,
+    -(y2 + width / 2)
+  );
+  drawLine(
+    ortaX + width / 2,
+    -y - width / 2,
+    ortaX + width / 2,
+    -(y2 + width / 2)
+  );
   // drawLine()
 }
 
 function prepare() {
+  ctx.clearRect(-width / 2, -height / 2, width, height);
   drawLine(0, 0, width / 2, 0);
   drawLine(0, 0, 0, height / 2);
   drawLine(0, 0, -width / 2, 0);
   drawLine(0, 0, 0, -height / 2);
 
-  drawCube(-150,-150,50, 150, 200);
+  drawCube(squareX1, squareX2, squareY, squareY + cubeH, cubeW);
 
   // drawPoints(points);
 }
 prepare();
+
+setInterval(() => {
+  if (keys["aLeft"]) {
+    squareX1 -= 2;
+    squareX2 -= 2;
+  }
+  if (keys["aRight"]) {
+    squareX1 += 2;
+    squareX2 += 2;
+  }
+  if (keys["aUp"]) {
+    squareY += 2;
+  }
+  if (keys["aDown"]) {
+    squareY -= 2;
+  }
+
+  if (keys["w"]) {
+    cubeH += 2;
+  }
+  if (keys["s"]) {
+    cubeH -= 2;
+  }
+  if (keys["e"]) {
+    cubeW += 2;
+  }
+  if (keys["q"]) {
+    cubeW -= 2;
+  }
+
+  prepare();
+}, 16.66);
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowLeft") {
+    keys["aLeft"] = true;
+  }
+  if (e.key === "ArrowRight") {
+    keys["aRight"] = true;
+  }
+  if (e.key === "ArrowUp") {
+    keys["aUp"] = true;
+  }
+  if (e.key === "ArrowDown") {
+    keys["aDown"] = true;
+  }
+  if (e.key === "w") {
+    keys["w"] = true;
+  }
+  if (e.key === "s") {
+    keys["s"] = true;
+  }
+  if (e.key === "e") {
+    keys["e"] = true;
+  }
+  if (e.key === "q") {
+    keys["q"] = true;
+  }
+});
+window.addEventListener("keyup", (e) => {
+  if (e.key === "ArrowLeft") {
+    keys["aLeft"] = false;
+  }
+  if (e.key === "ArrowRight") {
+    keys["aRight"] = false;
+  }
+  if (e.key === "ArrowUp") {
+    keys["aUp"] = false;
+  }
+  if (e.key === "ArrowDown") {
+    keys["aDown"] = false;
+  }
+  if (e.key === "w") {
+    keys["w"] = false;
+  }
+  if (e.key === "s") {
+    keys["s"] = false;
+  }
+  if (e.key === "e") {
+    keys["e"] = false;
+  }
+  if (e.key === "q") {
+    keys["q"] = false;
+  }
+});
