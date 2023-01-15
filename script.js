b436d7443bf8a82e650a3b2be338a402ea954716
xFarisSx <faristaleek@gmail.com>
2023-01-15 17:53:36 +0300

    3d cube

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ let y1 = 0;
 let color = "white";
 ctx.translate(width / 2, height / 2);
 ctx2.translate(width / 2, height / 2);
-let points = [];
+// let points = [];
 
 function drawCircle(x, y, size2, color2) {
   ctx.beginPath();
@@ -66,13 +66,41 @@ function drawPoints(points) {
   }
 }
 
-function makePoints(start, end) {
+function makePoints(start, end, func) {
+  let points = [];
   for (let x = start; x <= end; x += 1) {
-    y = (x * x) / 16;
-    points.push([x * 15, y * 15]);
+    y = func(x);
+    points.push([x, y]);
   }
   x1 = points[0][0];
   y1 = points[0][1];
+  drawPoints(points);
+}
+
+function drawSquare(x, y, sWidth, front) {
+  let oran = 1;
+  makePoints(x, x + sWidth / (oran * 2), (x) => {
+    return x + y;
+  });
+  if (front) {
+    makePoints(x - sWidth / (oran * 2), x, (x) => {
+      return x + sWidth + y;
+    });
+    makePoints(x, x + sWidth / (oran * 2), (x) => {
+      return -x + sWidth + y;
+    });
+  }
+  makePoints(x - sWidth / (oran * 2), x, (x) => {
+    return -x + y;
+  });
+}
+
+function drawCube(y, width) {
+  drawSquare(0, 0, width, false);
+  drawSquare(0, y, width, true);
+  drawLine(-width / 2, -width / 2 - y, -width / 2, -Math.abs(width / 2));
+  drawLine(width / 2, -width / 2 - y, width / 2, -Math.abs(width / 2));
+  // drawLine()
 }
 
 function prepare() {
@@ -81,152 +109,8 @@ function prepare() {
   drawLine(0, 0, -width / 2, 0);
   drawLine(0, 0, 0, -height / 2);
 
-  makePoints(-width / 2, width / 2);
+  drawCube(100, 100);
 
-  drawPoints(points);
+  // drawPoints(points);
 }
 prepare();
-
-function drawLines(pos) {
-  ctx.save();
-
-  ctx.shadowColor = "red";
-  ctx.shadowBlur = 15;
-  let di = Math.abs(-pos[1] - pos[0]) / Math.sqrt(2);
-  let hip2 = pos[0] * pos[0] + -pos[1] * -pos[1];
-  let z = Math.sqrt(hip2 / 2 - (di * di) / 2);
-  color = "red";
-  pos[1] > pos[0] ? drawLine(0, 0, -z, z) : drawLine(0, 0, z, -z);
-  color = "red";
-  drawLine(0, 0, pos[0], 0);
-  color = "red";
-  drawLine(0, 0, 0, pos[1]);
-  color = "red";
-  drawLine(0, 0, pos[0], pos[1]);
-
-  drawCircle(pos[0], pos[1], 10, "white");
-  ctx.restore();
-}
-
-function drawVectors(e) {
-  init();
-
-  color = "red";
-  let pos = [e.clientX - width / 2, e.clientY - height / 2];
-  drawLine(x, y, pos[0], pos[1], 10, "white");
-
-  if (keys["keyL"]) {
-    console.log("wow");
-    ctx2.beginPath();
-    ctx2.shadowColor = "red";
-    ctx2.shadowBlur = 5;
-    ctx2.fillStyle = "white";
-    ctx2.arc(pos[0], pos[1], 10, 0, Math.PI * 2);
-    ctx2.fill();
-    drawLine(x, y, pos[0], pos[1], 10, "white");
-
-    ctx2.beginPath();
-    ctx2.moveTo(x, y);
-    ctx2.lineTo(pos[0], pos[1]);
-    ctx2.lineCap = "round";
-    ctx2.strokeStyle = "white";
-    ctx2.lineWidth = 10 * 2;
-    ctx2.stroke();
-    color = "white";
-  } else if (keys["keyR"]) {
-    clearRoundRect(ctx2, pos[0] - 12.5, pos[1] - 12.5, 25, 25, 15);
-  } else if (keys["keyM"]) {
-    ctx.save();
-    ctx.shadowColor = "red";
-    ctx.shadowBlur = 15;
-    ctx.beginPath();
-    ctx.moveTo(pressPos[0], pressPos[1]);
-    ctx.lineTo(pos[0], pos[1]);
-    ctx.lineCap = "round";
-    ctx.strokeStyle = "rgba(255,255,255,0.8)";
-    ctx.lineWidth = 10 * 2;
-    ctx.stroke();
-    ctx.restore();
-  }
-
-  x = pos[0];
-  y = pos[1];
-  drawLines(pos);
-}
-document.addEventListener(
-  "contextmenu",
-  (event) => {
-    event.preventDefault();
-    // keys['keyR'] = false
-  },
-  false
-);
-
-function clearRoundRect(context, x, y, width, height, radius) {
-  context.save();
-  context.beginPath();
-  context.roundRect(x, y, width, height, radius, false, true);
-  context.clip();
-  context.clearRect(x, y, width, height);
-  context.restore();
-}
-
-window.addEventListener("mousedown", (e) => {
-  pressed = true;
-  // e.preventDefault();
-  if (e.button == 0) {
-    keys["keyL"] = true;
-  }
-  if (e.button == 2) {
-    keys["keyR"] = true;
-  }
-  if (e.button == 1) {
-    keys["keyM"] = true;
-    pressPos = [x, y];
-  }
-});
-window.addEventListener("mouseup", (e) => {
-  // e.preventDefault();
-  pressed = false;
-  if (e.button == 0) {
-    keys["keyL"] = false;
-  }
-  if (e.button == 2) {
-    keys["keyR"] = false;
-  }
-  if (e.button == 1) {
-    keys["keyM"] = false;
-    ctx2.shadowColor = "red";
-    ctx2.shadowBlur = 15;
-    ctx2.beginPath();
-    ctx2.moveTo(pressPos[0], pressPos[1]);
-    ctx2.lineTo(x, y);
-    ctx2.lineCap = "round";
-    ctx2.strokeStyle = "white";
-    ctx2.lineWidth = 10 * 2;
-    ctx2.stroke();
-  }
-});
-window.addEventListener("mousemove", drawVectors);
-// window.addEventListener('resize', init)
-setInterval(() => {
-  ctx.save();
-  ctx.shadowColor = "red";
-  ctx.shadowBlur = 15;
-  ctx.restore();
-  if (keys['keyM']) {
-    init()
-
-  }
-  drawLines([x, y]);
-  ctx.fillStyle = "rgba(0,0,0,0.05)";
-  ctx.rect(-width / 2, -height / 2, width, height);
-  ctx.fill();
-}, 10);
-
-window.addEventListener("keypress", (e) => {
-  if (e.key == "c") {
-    ctx.clearRect(-width / 2, -height / 2, width, height);
-    ctx2.clearRect(-width / 2, -height / 2, width, height);
-  }
-});
